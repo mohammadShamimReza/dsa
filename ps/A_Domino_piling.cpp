13b701292fb3730f08e4b632ef7c34443d9b9808
Md Shamim Reza <mohammadshamimreza23393@gmail.com>
2025-11-25 14:45:20 +0600

    solve domino piling

---
+++ ps/A_Domino_piling.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main (){
+    int n, m;
+    cin >> m >> n;
+    int res = m * n/2;
+    // int root = sqrt(res);
+    // cout << root;
+    int r = static_cast<int>(res);
+    cout << r;
+    return 0;
+}
