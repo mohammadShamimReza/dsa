51e0aaab8242f3136b045f97bc5badaaa8cd95c5
Md Shamim Reza <mohammadshamimreza23393@gmail.com>
2025-11-26 08:46:37 +0600

    solved 2 problem in cpp

---
+++ ps/A_Beautiful_Matrix.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+    int l, f;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            int x;
+
+            cin >> x;
+            if(x == 1){
+                l = i;
+                f = j;
+
+                cout << abs(l - 2) + abs(f - 2);
+
+                return 0;
+            }
+        }
+    }
+
+  
+
+
+}
