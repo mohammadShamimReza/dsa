8dce6870b2c7b70469e13d88523672b7ee34f28b
Md Shamim Reza <mohammadshamimreza23393@gmail.com>
2026-03-04 19:48:38 +0600

    setinterval, clearinterval

---
+++ javascript/setInterval.js
@@ -0,0 +1,18 @@
+// setInterval((p1, p2) => {console.log("calling"), console.log(p1 + p2)}, 1000, 1,3)
+
+// myArray = ["zero", "one", "two"];
+
+// myArray.myMethod = function (sProperty) {
+//     console.log(sProperty,'this is ')
+//   console.log(arguments.length > 0 ? this[sProperty] : this, 'this is two');
+// };
+
+// // myArray.myMethod(); // prints "zero,one,two"
+// // myArray.myMethod(1); // prints "one"
+// setTimeout(myArray.myMethod, 1000); // Alerts "[object Window]" after 1 second
+// setTimeout(myArray.myMethod, 1500, "1"); // Alerts "undefined" after 1.5 seconds
+
+
+// setTimeout(() => myArray.myMethod(1), 1500); // Alert "one" after 1.5 seconds
+// setTimeout(() => myArray.myMethod(2), 3000); // Alert "one" after 3 seconds
+
